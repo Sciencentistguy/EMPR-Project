1fc1d080980de2936e1701187544074a5175effa
Rory <rory@jragon.co.uk>
2020-01-23 15:58:01 +0000

    move tests to tests

--- test_code/movement_test.c
+++ test_code/movement_test.c
@@ -6,7 +6,12 @@
 #include "libs/systick_delay.h"
 #include "libs/motors.h"
 
-
+Motor_t motor_z = {
+    MOTOR_ZPEN_LATCH_ADDRESS,
+    MOTOR_STEPX,
+    SWITCH_Z_MASK,
+    0
+};
 
 int main() {
     serial_init();
@@ -17,12 +22,14 @@ int main() {
 
     int xsteps = 1000 - movex(-1000);
     int ysteps = 1000 - movey(-1000);
+    int zsteps = 1000 - move(&motor_z, -1000);
 
     serial_printf("home steps x: %d\r\n", xsteps);
     serial_printf("home steps y: %d\r\n", ysteps);
 
     movex(xsteps);
     movey(ysteps);
+    move(&motor_z, zsteps);
 
     return 0;
 }
