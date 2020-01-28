37c0066a971328d5d0ad32baa8803195ebc94775
Rory <rory@jragon.co.uk>
2020-01-28 14:30:24 +0000

    add basic sensor config

--- main.c
+++ main.c
@@ -7,48 +7,26 @@
 #include "libs/systick_delay.h"
 #include "libs/motors.h"
 #include "libs/grid.h"
+#include "libs/sensor.h"
+#include "libs/sensor_commands.h"
 
 int main() {
     serial_init();
     systick_init();
     i2c_init();
-    setup_switches();
 
     serial_printf("hello\r\n");
 
-    Grid_t grid = {
-        950, 950, 10, 260, 0, 0
-    };
+    sensor_enable();
 
-
-    grid_home(&grid);
-
-    systick_delay_blocking(100);
-
-    uint16_t offset_x = 400;
-    uint16_t offset_y = 400;
-    uint16_t radius = 200;
+    uint16_t colours[4] = { 0 };
 
     while(1) {
-        for (int theta = 0; theta < 360; theta++) {
-            double rad = theta * 3.14/180;
-            int x = offset_x + (radius * sin(rad));
-            int y = offset_y + (radius * cos(rad));
-            grid_move_to_point(&grid, x, y);
-            // serial_printf("x: %3d, y: %3d\r\n", x, y);
-        }
+        sensor_read_all_colours(colours);
+        serial_printf("C: %d, R: %d, G: %d, B: %d\r\n", colours[0], colours[1], colours[2], colours[3]);
+
+        systick_delay_blocking(25);
     }
-
-    // movex(900);
-
-    // raster
-    // while (grid.y < grid.max_y) {
-    //     grid_move_to_point(&grid, grid.max_x, grid.y);
-    //     systick_delay_blocking(100);
-    //     grid_move_to_point(&grid, 0, grid.y + 5);
-    // }
-
-
 
     return 0;
 }
