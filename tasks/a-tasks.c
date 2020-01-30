0049334e3f70e8f28e3fce55493daecae0d7d877
Rory <rory@jragon.co.uk>
2020-01-30 15:43:39 +0000

    use static grid var

--- tasks/a-tasks.c
+++ tasks/a-tasks.c
@@ -1,10 +1,10 @@
 #include <lpc_types.h>
 
-#include "../libs/keypad.h"
-#include "../libs/serial.h"
 #include "../libs/grid.h"
+#include "../libs/keypad.h"
 #include "../libs/lcd.h"
 #include "../libs/motors.h"
+#include "../libs/serial.h"
 #include "../libs/systick_delay.h"
 
 void a3_manual_move() {
@@ -14,11 +14,7 @@ void a3_manual_move() {
 
     lcd_printf(0x00, "homing");
 
-    // perhaps make grid a singleton
-    Grid_t grid = {
-        700, 700, 10, 260, 0, 0
-    };
-    grid_home(&grid);
+    grid_home();
 
     lcd_printf(0x00, "Press 1-6");
     lcd_printf(0x40, "(%3d, %3d, %3d)", 0, 0, 0);
@@ -30,32 +26,31 @@ void a3_manual_move() {
         }
 
         char k = keypad_read();
-        switch (k)
-        {
-        case '1':
-            grid_x_steps(&grid, 8);
-            break;
-        case '4':
-            grid_x_steps(&grid, -8);
-            break;
-        case '2':
-            grid_y_steps(&grid, 8);
-            break;
-        case '5':
-            grid_y_steps(&grid, -8);
-            break;
-        case '3':
-            movez(5);
-            break;
-        case '6':
-            movez(-5);
-            break;
-        default:
-            continue;
-            break;
+        switch (k) {
+            case '1':
+                grid_x_steps(8);
+                break;
+            case '4':
+                grid_x_steps(-8);
+                break;
+            case '2':
+                grid_y_steps(8);
+                break;
+            case '5':
+                grid_y_steps(-8);
+                break;
+            case '3':
+                movez(5);
+                break;
+            case '6':
+                movez(-5);
+                break;
+            default:
+                continue;
+                break;
         }
 
         systick_delay_flag_reset();
-        lcd_printf(0x40, "(%3d, %3d, %3d)", grid.x, grid.y, 0);
+        lcd_printf(0x40, "(%3d, %3d, %3d)", grid_get_x(), grid_get_y(), 0);
     }
 }
--- tasks/b-tasks.c
+++ tasks/b-tasks.c
@@ -1,38 +1,37 @@
 #include <lpc_types.h>
 
-#include "../libs/keypad.h"
-#include "../libs/serial.h"
 #include "../libs/grid.h"
+#include "../libs/keypad.h"
 #include "../libs/lcd.h"
 #include "../libs/motors.h"
-#include "../libs/systick_delay.h"
 #include "../libs/sensor.h"
+#include "../libs/serial.h"
+#include "../libs/systick_delay.h"
 
-uint8_t _manmove(Grid_t *grid) {
+uint8_t _manmove() {
     char k = keypad_read();
-    switch (k)
-    {
-    case '1':
-        grid_x_steps(grid, 8);
-        break;
-    case '4':
-        grid_x_steps(grid, -8);
-        break;
-    case '2':
-        grid_y_steps(grid, 8);
-        break;
-    case '5':
-        grid_y_steps(grid, -8);
-        break;
-    case '3':
-        movez(5);
-        break;
-    case '6':
-        movez(-5);
-        break;
-    default:
-        return -1;
-        break;
+    switch (k) {
+        case '1':
+            grid_x_steps(8);
+            break;
+        case '4':
+            grid_x_steps(-8);
+            break;
+        case '2':
+            grid_y_steps(8);
+            break;
+        case '5':
+            grid_y_steps(-8);
+            break;
+        case '3':
+            movez(5);
+            break;
+        case '6':
+            movez(-5);
+            break;
+        default:
+            return -1;
+            break;
     }
 
     return 0;
@@ -45,10 +44,7 @@ void b1_xyz_move_rgb() {
     lcd_printf(0x00, "homing");
 
     // perhaps make grid a singleton
-    Grid_t grid = {
-        700, 700, 10, 260, 0, 0
-    };
-    grid_home(&grid);
+    grid_home();
 
     uint16_t rgb_vals[4] = {0};
     sensor_read_all_colours(rgb_vals);
@@ -63,13 +59,14 @@ void b1_xyz_move_rgb() {
             continue;
         }
 
-        if (_manmove(&grid) == -1) {
+        if (_manmove() == -1) {
             continue;
         }
 
         systick_delay_flag_reset();
 
         sensor_read_all_colours(rgb_vals);
-        lcd_printf(0x40, "(%3d, %3d, %3d)", rgb_vals[1] >> 8, rgb_vals[2] >> 8, rgb_vals[3] >> 8);
+        lcd_printf(0x40, "(%3d, %3d, %3d)", rgb_vals[1] >> 8, rgb_vals[2] >> 8,
+                   rgb_vals[3] >> 8);
     }
 }
